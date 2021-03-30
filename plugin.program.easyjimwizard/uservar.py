4ea76683fcb2474e908db564094e1c2e5ff75aff
James <4528123+fungusfeet@users.noreply.github.com>
2021-03-30 19:41:33 +0100

    grrrr

--- plugin.program.easyjimwizard/uservar.py
+++ plugin.program.easyjimwizard/uservar.py
@@ -17,7 +17,7 @@ ADDONTITLE = '[COLOR limegreen][B]EasyJim[/B][/COLOR]Wizard'
 BUILDERNAME = 'easyjim'
 EXCLUDES = [ADDON_ID, 'repository.easyjim']
 # Text File with build info in it.
-BUILDFILE = 'https://raw.githubusercontent.com/easyjim/repo/master/zips/builds.txt'
+BUILDFILE = 'https://raw.githubusercontent.com/easyjim/easyjim.github.io/master/zips/builds.txt'
 # How often you would like it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK = 0
@@ -77,7 +77,7 @@ THEME5 = u'[COLOR {color1}]Current Theme:[/COLOR] [COLOR {color2}]{{}}[/COLOR]'.
 # Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
 HIDECONTACT = 'No'
 # You can add \n to do line breaks
-CONTACT = 'Thank you for choosing EasyJimWizard.\n\nContact us on Github at http://www.github.com/easyjim/plugin.program.openwizard/'
+CONTACT = 'Thank you for choosing EasyJimWizard.\n\nContact us on Github at https://github.com/easyjim/easyjim.github.io/tree/master/plugin.program.easyjimwizard'
 # Images used for the contact window.  http:// for default icon and fanart
 CONTACTICON = os.path.join(ART, 'qricon.png')
 CONTACTFANART = 'http://'
@@ -98,9 +98,9 @@ AUTOINSTALL = 'Yes'
 # Addon ID for the repository
 REPOID = 'repository.easyjim'
 # Url to Addons.xml file in your repo folder(this is so we can get the latest version)
-REPOADDONXML = 'https://raw.githubusercontent.com/easyjim/repo/master/zips/repository.easyjim/addon.xml'
+REPOADDONXML = 'https://raw.githubusercontent.com/easyjim/easyjim.github.io/master/zips/repository.easyjim/addon.xml'
 # Url to folder zip is located in
-REPOZIPURL = 'https://raw.githubusercontent.com/easyjim/repo/master/zips/repository.easyjim/'
+REPOZIPURL = 'https://raw.githubusercontent.com/easyjim/easyjim.github.io/master/zips/repository.easyjim/'
 #########################################################
 
 #########################################################
@@ -109,7 +109,7 @@ REPOZIPURL = 'https://raw.githubusercontent.com/easyjim/repo/master/zips/reposit
 # Enable Notification screen Yes or No
 ENABLE = 'Yes'
 # Url to notification file
-NOTIFICATION = 'https://raw.githubusercontent.com/easyjim/repo/master/zips/notify.txt'
+NOTIFICATION = 'https://raw.githubusercontent.com/easyjim/easyjim.github.io/master/zips/notify.txt'
 # Use either 'Text' or 'Image'
 HEADERTYPE = 'Text'
 # Font size of header
